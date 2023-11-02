416f7dbddaaa0e3c1926763751f4b902ba9dedd0
Yana <73012693+YanochkaT@users.noreply.github.com>
2023-11-02 09:25:34 +0200

    Create AboutItem.js

---
+++ src/components/AboutItem.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../css/index.css';
+
+const AboutItem = () => {
+    return(
+        <div>
+        <div className='about_item'>
+            <p> About Item</p>
+        </div>
+        </div>
+    )
+}
+
+export default AboutItem;
