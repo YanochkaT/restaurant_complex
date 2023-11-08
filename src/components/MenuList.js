eb1c68df474c690c7e1aaa46a6c5f1dec0d6ac66
Yana <73012693+YanochkaT@users.noreply.github.com>
2023-11-08 22:05:08 +0200

    Update MenuList.js

--- src/components/MenuList.js
+++ src/components/MenuList.js
@@ -3,21 +3,21 @@ import '../css/index.css';
 
 const MenuList = (props) => {
     return(
-        <div>
-            <div>
-            <div className='menu_cont'>
+        <div className='menu_box'>    
+           
             <div className='menu_photo'>
                 <img src="../src/img/src/img/400_0_1669036584-8046.jpg" alt= "NO"></img>
-
             </div>
+
+
             <p className='text_menu_list'>{props.text1}</p>
-            <p className='price'>{props.text2}</p>
+            <p className='price'>{props.text2} грн</p>
+
 
             <div className='button_menu'>
                 <button className='button_text'>Додати</button>
             </div>
-        </div>
-        </div>
+        
         </div>
     )
 }
