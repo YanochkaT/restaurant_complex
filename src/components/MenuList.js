521800348f2303be6d3d2534299af2b6a7e7d82d
Yana <73012693+YanochkaT@users.noreply.github.com>
2023-11-22 22:05:31 +0200

    Update MenuList.js

--- src/components/MenuList.js
+++ src/components/MenuList.js
@@ -1,22 +1,36 @@
 import React from 'react';
 import '../css/index.css';
 
+// import image1 from '../images/restaurant_food/1_4гавайська-445x445.jpg';
+//       <img src= {image1} alt="N"/>
+
+
 const MenuList = (props) => {
     return(
         <div className='menu_box'>    
            
-            <div className='menu_photo'>
-                <img src="../src/img/src/img/400_0_1669036584-8046.jpg" alt= "NO"></img>
+            <div className='menu_photo'></div>
+
+
+            <p className='text_menu_name'>{props.text1}</p>
+
+
+
+            <div className='text_menu_content'>
+                <div className='text_menu_content_left'>
+            <p className='item_price'>{props.text2} грн</p>
+
+            <div className='menu_content_weight_container'>
+            <p className='text_menu_weight'>{props.text4} г</p>
+            </div>
+                </div>
+
+            <div className='text_menu_content_right'>
+                <p>{props.text3} </p>
+            </div>
+            
             </div>
 
-
-            <p className='text_menu_list'>{props.text1}</p>
-            <p className='price'>{props.text2} грн</p>
-
-
-            <div className='button_menu'>
-                <button className='button_text'>Додати</button>
-            </div>
         
         </div>
     )
