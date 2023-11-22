f3a95f4f2c717879c8ee9afc2f7c90dda1ace357
Yana <73012693+YanochkaT@users.noreply.github.com>
2023-11-22 22:04:43 +0200

    Update Categories.js

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -4,15 +4,17 @@ import '../css/index.css';
 const Categories = (props) => {
     return(
         <div className='category_button_container'>
-        <button className='category_box'>
+    
+       
             <div class="image_category">
 
             </div>
 
                 <div class="text_category">
              <p class="pizza">{props.ctext1}</p>
-                </div>    
-        </button>
+                </div>   
+             
+     
         </div>
     )
 }
