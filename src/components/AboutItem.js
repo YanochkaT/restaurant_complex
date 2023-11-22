7f3553697e3df0663d2cad63df59071f7dd26c37
Yana <73012693+YanochkaT@users.noreply.github.com>
2023-11-22 22:04:16 +0200

    Update AboutItem.js

--- src/components/AboutItem.js
+++ src/components/AboutItem.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import '../css/index.css';
 
-const AboutItem = () => {
+const AboutItem = (props) => {
     return(
-        <div>
-        <div className='about_item'>
-            <p> About Item</p>
+        <div className='service_item'>
+
+        <div className='service_item_left'>
+        <div className='service_item_name'>
+    <h2 className='text_menu_name'  font-weight = 'bold' >{props.text1}</h2>
         </div>
+
+    <p className='item_price'>{props.text2} </p>
+    <p className='item_price'>{props.text3} </p>
         </div>
+
+        <div className='service_item_right'>
+        </div>
+
+
+    </div>
+        
     )
 }
-
 export default AboutItem;
+
