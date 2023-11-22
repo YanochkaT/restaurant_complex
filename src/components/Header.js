d3497ebdc587ad604b0864ec77b0bfdb7d7083f1
Yana <73012693+YanochkaT@users.noreply.github.com>
2023-11-22 22:05:11 +0200

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,21 +9,24 @@ function Header() {
     <div className="header_bottom_box">
 
     <div className="header_top_box">
+
       <div className="logo_container">
-        <img src="C:\Users\Admin\Desktop\burger_logo.png" alt="Website Logo"/>
+        <Link to='/main'>
+          <div className="logo_container_fill"></div>
+        </Link>
       </div>
 
       <ul className="navigation">
         <li><Link to='/main'>Головне Меню</Link></li>
         <li><Link to="/services">Святкові Послуги</Link></li>
         <li><Link to="/about">Про Нас</Link></li>
-        <li><Link to="/zamovlennya">Оформити Замовлення</Link></li>
+        <li><Link to="/shopcart">Оформити Замовлення</Link></li>
+
       </ul>
      
 
       <div className="profile_container">
-        <button className="button_text"><Link to="/user">Login</Link></button>
-        <img src="" alt=""></img>
+        <button className="button_text"><Link to="/Register">Реєстрація</Link></button>
       </div>
 
     </div>
